306eb91fa05e673b0c2738060f5651da6e458483
轶澪阖羽 <25280272+UntaughtMinds@users.noreply.github.com>
2021-01-25 19:01:40 +0800

    完成基本功能

---
+++ calculator/MainForm.Designer.cs
@@ -0,0 +1,356 @@
+﻿
+namespace Calculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but9 = new System.Windows.Forms.Button();
+            this.lblExp = new System.Windows.Forms.Label();
+            this.lblRes = new System.Windows.Forms.Label();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but7 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but1 = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.butDot = new System.Windows.Forms.Button();
+            this.but0 = new System.Windows.Forms.Button();
+            this.butNagate = new System.Windows.Forms.Button();
+            this.butRes = new System.Windows.Forms.Button();
+            this.butAdd = new System.Windows.Forms.Button();
+            this.butSub = new System.Windows.Forms.Button();
+            this.butMult = new System.Windows.Forms.Button();
+            this.butDiv = new System.Windows.Forms.Button();
+            this.butDel = new System.Windows.Forms.Button();
+            this.butC = new System.Windows.Forms.Button();
+            this.butCE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // but9
+            // 
+            this.but9.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but9.Location = new System.Drawing.Point(19, 266);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(70, 70);
+            this.but9.TabIndex = 0;
+            this.but9.Text = "9";
+            this.but9.UseVisualStyleBackColor = true;
+            this.but9.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // lblExp
+            // 
+            this.lblExp.Font = new System.Drawing.Font("Microsoft YaHei UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblExp.Location = new System.Drawing.Point(19, 22);
+            this.lblExp.Name = "lblExp";
+            this.lblExp.Size = new System.Drawing.Size(298, 23);
+            this.lblExp.TabIndex = 1;
+            this.lblExp.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblExp.Click += new System.EventHandler(this.lblExp_Click);
+            // 
+            // lblRes
+            // 
+            this.lblRes.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblRes.Location = new System.Drawing.Point(19, 88);
+            this.lblRes.Name = "lblRes";
+            this.lblRes.Size = new System.Drawing.Size(298, 45);
+            this.lblRes.TabIndex = 2;
+            this.lblRes.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblRes.Click += new System.EventHandler(this.lblRes_Click);
+            // 
+            // but8
+            // 
+            this.but8.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but8.Location = new System.Drawing.Point(95, 266);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(70, 70);
+            this.but8.TabIndex = 3;
+            this.but8.Text = "8";
+            this.but8.UseVisualStyleBackColor = true;
+            this.but8.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // but7
+            // 
+            this.but7.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but7.Location = new System.Drawing.Point(171, 266);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(70, 70);
+            this.but7.TabIndex = 4;
+            this.but7.Text = "7";
+            this.but7.UseVisualStyleBackColor = true;
+            this.but7.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // but4
+            // 
+            this.but4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but4.Location = new System.Drawing.Point(171, 342);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(70, 70);
+            this.but4.TabIndex = 7;
+            this.but4.Text = "4";
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // but5
+            // 
+            this.but5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but5.Location = new System.Drawing.Point(95, 342);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(70, 70);
+            this.but5.TabIndex = 6;
+            this.but5.Text = "5";
+            this.but5.UseVisualStyleBackColor = true;
+            this.but5.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // but6
+            // 
+            this.but6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but6.Location = new System.Drawing.Point(19, 342);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(70, 70);
+            this.but6.TabIndex = 5;
+            this.but6.Text = "6";
+            this.but6.UseVisualStyleBackColor = true;
+            this.but6.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // but1
+            // 
+            this.but1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but1.Location = new System.Drawing.Point(171, 418);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(70, 70);
+            this.but1.TabIndex = 10;
+            this.but1.Text = "1";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // but2
+            // 
+            this.but2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but2.Location = new System.Drawing.Point(95, 418);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(70, 70);
+            this.but2.TabIndex = 9;
+            this.but2.Text = "2";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // but3
+            // 
+            this.but3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but3.Location = new System.Drawing.Point(19, 418);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(70, 70);
+            this.but3.TabIndex = 8;
+            this.but3.Text = "3";
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // butDot
+            // 
+            this.butDot.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butDot.Location = new System.Drawing.Point(171, 494);
+            this.butDot.Name = "butDot";
+            this.butDot.Size = new System.Drawing.Size(70, 70);
+            this.butDot.TabIndex = 13;
+            this.butDot.Text = ".";
+            this.butDot.UseVisualStyleBackColor = true;
+            this.butDot.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // but0
+            // 
+            this.but0.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.but0.Location = new System.Drawing.Point(95, 494);
+            this.but0.Name = "but0";
+            this.but0.Size = new System.Drawing.Size(70, 70);
+            this.but0.TabIndex = 12;
+            this.but0.Text = "0";
+            this.but0.UseVisualStyleBackColor = true;
+            this.but0.Click += new System.EventHandler(this.butNum_Click);
+            // 
+            // butNagate
+            // 
+            this.butNagate.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butNagate.Location = new System.Drawing.Point(19, 494);
+            this.butNagate.Name = "butNagate";
+            this.butNagate.Size = new System.Drawing.Size(70, 70);
+            this.butNagate.TabIndex = 11;
+            this.butNagate.Text = "+/-";
+            this.butNagate.UseVisualStyleBackColor = true;
+            this.butNagate.Click += new System.EventHandler(this.butNagate_Click);
+            // 
+            // butRes
+            // 
+            this.butRes.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butRes.Location = new System.Drawing.Point(247, 494);
+            this.butRes.Name = "butRes";
+            this.butRes.Size = new System.Drawing.Size(70, 70);
+            this.butRes.TabIndex = 17;
+            this.butRes.Text = "=";
+            this.butRes.UseVisualStyleBackColor = true;
+            this.butRes.Click += new System.EventHandler(this.butRes_Click);
+            // 
+            // butAdd
+            // 
+            this.butAdd.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butAdd.Location = new System.Drawing.Point(247, 418);
+            this.butAdd.Name = "butAdd";
+            this.butAdd.Size = new System.Drawing.Size(70, 70);
+            this.butAdd.TabIndex = 16;
+            this.butAdd.Text = "+";
+            this.butAdd.UseVisualStyleBackColor = true;
+            this.butAdd.Click += new System.EventHandler(this.butOperate_Click);
+            // 
+            // butSub
+            // 
+            this.butSub.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butSub.Location = new System.Drawing.Point(247, 342);
+            this.butSub.Name = "butSub";
+            this.butSub.Size = new System.Drawing.Size(70, 70);
+            this.butSub.TabIndex = 15;
+            this.butSub.Text = "-";
+            this.butSub.UseVisualStyleBackColor = true;
+            this.butSub.Click += new System.EventHandler(this.butOperate_Click);
+            // 
+            // butMult
+            // 
+            this.butMult.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butMult.Location = new System.Drawing.Point(247, 266);
+            this.butMult.Name = "butMult";
+            this.butMult.Size = new System.Drawing.Size(70, 70);
+            this.butMult.TabIndex = 14;
+            this.butMult.Text = "×";
+            this.butMult.UseVisualStyleBackColor = true;
+            this.butMult.Click += new System.EventHandler(this.butOperate_Click);
+            // 
+            // butDiv
+            // 
+            this.butDiv.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butDiv.Location = new System.Drawing.Point(247, 190);
+            this.butDiv.Name = "butDiv";
+            this.butDiv.Size = new System.Drawing.Size(70, 70);
+            this.butDiv.TabIndex = 21;
+            this.butDiv.Text = "÷";
+            this.butDiv.UseVisualStyleBackColor = true;
+            this.butDiv.Click += new System.EventHandler(this.butOperate_Click);
+            // 
+            // butDel
+            // 
+            this.butDel.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butDel.Location = new System.Drawing.Point(171, 190);
+            this.butDel.Name = "butDel";
+            this.butDel.Size = new System.Drawing.Size(70, 70);
+            this.butDel.TabIndex = 20;
+            this.butDel.Text = "DEL";
+            this.butDel.UseVisualStyleBackColor = true;
+            this.butDel.Click += new System.EventHandler(this.butDel_Click);
+            // 
+            // butC
+            // 
+            this.butC.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butC.Location = new System.Drawing.Point(95, 190);
+            this.butC.Name = "butC";
+            this.butC.Size = new System.Drawing.Size(70, 70);
+            this.butC.TabIndex = 19;
+            this.butC.Text = "C";
+            this.butC.UseVisualStyleBackColor = true;
+            this.butC.Click += new System.EventHandler(this.butC_Click);
+            // 
+            // butCE
+            // 
+            this.butCE.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.butCE.Location = new System.Drawing.Point(19, 190);
+            this.butCE.Name = "butCE";
+            this.butCE.Size = new System.Drawing.Size(70, 70);
+            this.butCE.TabIndex = 18;
+            this.butCE.Text = "CE";
+            this.butCE.UseVisualStyleBackColor = true;
+            this.butCE.Click += new System.EventHandler(this.butCE_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 582);
+            this.Controls.Add(this.butDiv);
+            this.Controls.Add(this.butDel);
+            this.Controls.Add(this.butC);
+            this.Controls.Add(this.butCE);
+            this.Controls.Add(this.butRes);
+            this.Controls.Add(this.butAdd);
+            this.Controls.Add(this.butSub);
+            this.Controls.Add(this.butMult);
+            this.Controls.Add(this.butDot);
+            this.Controls.Add(this.but0);
+            this.Controls.Add(this.butNagate);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.but4);
+            this.Controls.Add(this.but5);
+            this.Controls.Add(this.but6);
+            this.Controls.Add(this.but7);
+            this.Controls.Add(this.but8);
+            this.Controls.Add(this.lblRes);
+            this.Controls.Add(this.lblExp);
+            this.Controls.Add(this.but9);
+            this.Name = "MainForm";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button but9;
+        private System.Windows.Forms.Label lblExp;
+        private System.Windows.Forms.Label lblRes;
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but5;
+        private System.Windows.Forms.Button but6;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button butDot;
+        private System.Windows.Forms.Button but0;
+        private System.Windows.Forms.Button butNagate;
+        private System.Windows.Forms.Button butRes;
+        private System.Windows.Forms.Button butAdd;
+        private System.Windows.Forms.Button butSub;
+        private System.Windows.Forms.Button butMult;
+        private System.Windows.Forms.Button butDiv;
+        private System.Windows.Forms.Button butDel;
+        private System.Windows.Forms.Button butC;
+        private System.Windows.Forms.Button butCE;
+    }
+}
+
---
+++ calculator/MainForm.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainForm : Form
+    {
+        private string currentNumberText = string.Empty;
+        private List<string> inputElement = new();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblRes_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(lblRes.Text);
+            MessageBox.Show("内容已复制");
+        }
+
+        private void lblExp_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(lblExp.Text);
+            MessageBox.Show("内容已复制");
+        }
+
+        private void butNagate_Click(object sender, EventArgs e)
+        {
+            if (currentNumberText != string.Empty)
+            {
+                inputElement.Add(currentNumberText);
+            }
+            if (inputElement.Count != 0)
+            {
+                inputElement.Add("×");
+            }
+            inputElement.Add("-1");
+            currentNumberText = string.Empty;
+            displaylblRes(currentNumberText);
+            displaylblExp();
+        }
+
+        private void butNum_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (currentNumberText != null && button.Text == "." && currentNumberText.Contains("."))
+            {
+                return;
+            }
+            currentNumberText += button.Text;
+            displaylblRes(currentNumberText);
+            displaylblExp();
+        }
+
+        private void butOperate_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (inputElement.Count == 0 && currentNumberText == string.Empty)
+            {
+                return;
+            }
+
+            if (inputElement.Count > 0 && currentNumberText == string.Empty &&
+                (inputElement.Last() == "+" ||
+                inputElement.Last() == "-" ||
+                inputElement.Last() == "×" ||
+                inputElement.Last() == "÷"))
+            {
+                inputElement[inputElement.Count - 1] = button.Text;
+            }
+            else
+            {
+                inputElement.Add(currentNumberText);
+                inputElement.Add(button.Text);
+            }
+
+            currentNumberText = string.Empty;
+            displaylblRes(cal().ToString());
+            displaylblExp();
+        }
+
+        private void butRes_Click(object sender, EventArgs e)
+        {
+            if (inputElement.Count == 0)
+            {
+                return;
+            }
+            inputElement.Add(currentNumberText);
+            inputElement.Add("=");
+            currentNumberText = string.Empty;
+            displaylblRes(cal());
+            displaylblExp();
+            inputElement.Clear();
+        }
+
+        private void butDel_Click(object sender, EventArgs e)
+        {
+            currentNumberText.Remove(currentNumberText.Length - 1, 1);
+            displaylblRes(currentNumberText);
+        }
+
+        private void butC_Click(object sender, EventArgs e)
+        {
+            currentNumberText = string.Empty;
+            inputElement.Clear();
+            displaylblRes(currentNumberText);
+            displaylblExp();
+        }
+
+        private void butCE_Click(object sender, EventArgs e)
+        {
+            currentNumberText = string.Empty;
+            displaylblRes(currentNumberText);
+        }
+
+        private String cal()
+        {
+            decimal tempNumber = 0.0M;
+            string currentOperator = string.Empty;
+            string res;
+
+            try
+            {
+                for (int i = 0; i < inputElement.Count - 1; i++)
+                {
+                    var item = inputElement[i];
+                    switch (item)
+                    {
+                        case "+":
+                        case "-":
+                        case "×":
+                        case "÷":
+                            currentOperator = item;
+                            break;
+                        default:
+                            switch (currentOperator)
+                            {
+                                case "+":
+                                    tempNumber += decimal.Parse(item);
+                                    break;
+                                case "-":
+                                    tempNumber -= decimal.Parse(item);
+                                    break;
+                                case "×":
+                                    tempNumber *= decimal.Parse(item);
+                                    break;
+                                case "÷":
+                                    tempNumber /= decimal.Parse(item);
+                                    break;
+                                default:
+                                    tempNumber = decimal.Parse(item);
+                                    break;
+                            }
+                            break;
+                    }
+                }
+                res = tempNumber.ToString();
+            }
+            catch (Exception)
+            {
+                res = "出现错误";
+                currentNumberText = string.Empty;
+                inputElement.Clear();
+                displaylblExp();
+            }
+            return res;
+        }
+
+        private void displaylblRes(string input)
+        {
+            lblRes.Text = input;
+        }
+
+        private void displaylblExp()
+        {
+            lblExp.Text = string.Join(" ", inputElement);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            // if it is a hotkey, return true; otherwise, return false
+            switch (keyData)
+            {
+                case Keys.NumPad0:
+                    but0.Focus();
+                    but0.PerformClick();
+                    return true;
+                case Keys.NumPad1:
+                    but1.Focus();
+                    but1.PerformClick();
+                    return true;
+                case Keys.NumPad2:
+                    but2.Focus();
+                    but2.PerformClick();
+                    return true;
+                case Keys.NumPad3:
+                    but3.Focus();
+                    but3.PerformClick();
+                    return true;
+                case Keys.NumPad4:
+                    but4.Focus();
+                    but4.PerformClick();
+                    return true;
+                case Keys.NumPad5:
+                    but5.Focus();
+                    but5.PerformClick();
+                    return true;
+                case Keys.NumPad6:
+                    but6.Focus();
+                    but6.PerformClick();
+                    return true;
+                case Keys.NumPad7:
+                    but7.Focus();
+                    but7.PerformClick();
+                    return true;
+                case Keys.NumPad8:
+                    but8.Focus();
+                    but8.PerformClick();
+                    return true;
+                case Keys.NumPad9:
+                    but9.Focus();
+                    but9.PerformClick();
+                    return true;
+                case Keys.Add:
+                    butAdd.Focus();
+                    butAdd.PerformClick();
+                    return true;
+                case Keys.Subtract:
+                    butSub.Focus();
+                    butSub.PerformClick();
+                    return true;
+                case Keys.Multiply:
+                    butMult.Focus();
+                    butMult.PerformClick();
+                    return true;
+                case Keys.Divide:
+                    butDiv.Focus();
+                    butDiv.PerformClick();
+                    return true;
+                case Keys.Enter:
+                    butRes.Focus();
+                    butRes.PerformClick();
+                    return true;
+                case Keys.Escape:
+                    butCE.Focus();
+                    butCE.PerformClick();
+                    return true;
+                case Keys.Decimal:
+                    butDot.Focus();
+                    butDot.PerformClick();
+                    return true;
+                default:
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
